ccd828a6dc296ae6f0036d9eee070c44bfee5069
liliyafedyurina <liliya.fedyurina@gmail.com>
2025-05-06 17:21:49 -0700

    deployment

--- analyzer/utils.py
+++ analyzer/utils.py
@@ -48,14 +48,19 @@ def simulate_colorblindness(color, colorblind_type):
 def analyze_website_for_colorblindness(url, colorblind_type):
     # Set up Selenium WebDriver
     options = Options()
-    options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-    options.add_argument('--headless=new')
-    options.add_argument('--no-sandbox')  # Disable sandbox for Chrome on Heroku
-    options.add_argument('--disable-dev-shm-usage')  # Avoids out-of-memory errors
-    options.add_argument('--disable-gpu')
-    options.add_argument('--window-size=1200,800')
+    options.binary_location = "/app/.apt/usr/bin/google-chrome"
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--remote-debugging-port=9222")
+
     
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver = webdriver.Chromedriver = webdriver.Chrome(
+    executable_path="/app/.chromedriver/bin/chromedriver",
+    options=options
+)
+
     
     try:
         # Load the webpage
